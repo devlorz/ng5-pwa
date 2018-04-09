b3afb2a49bc89987e07fa4d33318af1bc2efda83
Nutti Saelor <leelorz6@gmail.com>
2018-04-10 01:44:34 +0700

    add manifest.json and fix caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,18 @@ export class AppComponent implements OnInit {
   constructor(private pokedexService: PokedexService) {}
 
   ngOnInit() {
-    this.pokedexService.getPokemon().subscribe(res => {
-      this.pokemonData = res;
-      this.pokemonList = res;
-    });
+    const saveData = localStorage.getItem('pokemonList');
+    if (saveData) {
+      const pokemonSaveData = JSON.parse(saveData);
+      this.pokemonData = pokemonSaveData;
+      this.pokemonList = pokemonSaveData;
+    } else {
+      this.pokedexService.getPokemon().subscribe(res => {
+        this.pokemonData = res;
+        this.pokemonList = res;
+        localStorage.setItem('pokemonList', JSON.stringify(res));
+      });
+    }
   }
 
   public onSearch(searchText: string) {

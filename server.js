152051147eb63c293ee42d228c2cb25f8873ed59
Nutti Saelor <leelorz6@gmail.com>
2018-05-04 00:00:43 +0700

    add pokemon api

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pokemon = require('./pokemon.json');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
@@ -6,3 +7,8 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/pokemon', function(req, res) {
+  const data = JSON.parse(pokemon);
+  res.status(200).send(data);
+});
